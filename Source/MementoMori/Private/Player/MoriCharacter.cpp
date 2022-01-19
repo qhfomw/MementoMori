230d3eb2b1f635a3ee426fcb8274a6ed7d5e5715
qhfomw <qhfomw@gmail.com>
2022-01-19 23:18:00 +0900

    Feat: Interact

--- Source/MementoMori/Private/Player/MoriCharacter.cpp
+++ Source/MementoMori/Private/Player/MoriCharacter.cpp
@@ -1,7 +1,11 @@
 #include "Player/MoriCharacter.h"
+#include "Interaction/MoriInteractionInterface.h"
+#include "Interaction/Weapon/MoriDroppedWeapon.h"
+#include "Interaction/Weapon/MoriPickedWeapon.h"
 
 #include "Camera/CameraComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -46,6 +50,18 @@ AMoriCharacter::AMoriCharacter()
 	GetCharacterMovement()->GetNavAgentPropertiesRef().bCanCrouch = true;
 }
 
+void AMoriCharacter::PickupWeapon(TSubclassOf<AMoriPickedWeapon> WeaponClass, int32 AmmoNum)
+{
+	ServerPickupWeapon(WeaponClass, AmmoNum);
+}
+
+void AMoriCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	ClientInit();
+}
+
 void AMoriCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -64,6 +80,22 @@ void AMoriCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 
 	PlayerInputComponent->BindAction(TEXT("Run"), IE_Pressed, this, &AMoriCharacter::Running);
 	PlayerInputComponent->BindAction(TEXT("Run"), IE_Released, this, &AMoriCharacter::Running);
+
+	PlayerInputComponent->BindAction(TEXT("Interact"), IE_Pressed, this, &AMoriCharacter::Interacting);
+	PlayerInputComponent->BindAction(TEXT("Interact"), IE_Released, this, &AMoriCharacter::StopInteracting);
+}
+
+void AMoriCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMoriCharacter, Target);
+}
+
+void AMoriCharacter::ClientInit_Implementation()
+{
+	if (auto* World = GetWorld())
+		World->GetTimerManager().SetTimer(CheckInteractionTimerHandle, this, &AMoriCharacter::CheckInteracting, 0.1f, true);
 }
 
 void AMoriCharacter::MoveForward(float AxisValue)
@@ -99,19 +131,20 @@ void AMoriCharacter::TogglePerspective()
 
 void AMoriCharacter::Jumping()
 {
-	bIsCrouched ? UnCrouch() : Jump();
+	bCrouching ? UnCrouch() : Jump();
+	bCrouching = false;
 }
 
 void AMoriCharacter::Crouching()
 {
-	if (!GetCharacterMovement()->IsFalling())
-	{
-		bCrouching = !bCrouching;
-		bCrouching ? Crouch() : UnCrouch();
+	if (GetCharacterMovement()->IsFalling())
+		return;
 
-		if (bRunning)
-			CheckRunning(!bCrouching);
-	}
+	bCrouching = !bCrouching;
+	bCrouching ? Crouch() : UnCrouch();
+
+	if (bRunning)
+		CheckRunning(!bCrouching);
 }
 
 void AMoriCharacter::Running()
@@ -143,4 +176,133 @@ void AMoriCharacter::CheckRunning(bool bHaveToRun)
 void AMoriCharacter::ServerRunning_Implementation(bool bHaveToRun)
 {
 	GetCharacterMovement()->MaxWalkSpeed = bHaveToRun ? 540.f : 360.f;
+}
+
+void AMoriCharacter::Interacting()
+{
+	bTryToInteract = true;
+
+	if (Target)
+		TryToInteract();
+}
+
+void AMoriCharacter::StopInteracting()
+{
+	bTryToInteract = false;
+
+	if (Target)
+		EndInteracting();
+}
+
+void AMoriCharacter::TryToInteract()
+{
+	if (auto* InteractedTarget = Cast<IMoriInteractionInterface>(Target))
+	{
+		if (!InteractedTarget->TryToInteract())
+			return;
+
+		BeginInteracting();
+
+		if (auto* World = GetWorld())
+		{
+			World->GetTimerManager().PauseTimer(CheckInteractionTimerHandle);
+			InteractedTarget->GetInteractionTime() ? World->GetTimerManager().SetTimer(InteractionTimerHandle, this, &AMoriCharacter::Interact, InteractedTarget->GetInteractionTime()) : Interact();
+		}
+	}
+}
+
+void AMoriCharacter::CheckInteracting()
+{
+	static AActor* PrevTarget = nullptr;
+
+	if (bRunning)
+		PrevTarget = Target = nullptr;
+
+	else if (auto* CameraManager = UGameplayStatics::GetPlayerCameraManager(this, 0))
+	{
+		FHitResult OutHit;
+		const auto& Start = CameraManager->GetCameraLocation() + (bUseFirstPersonView ? FVector(0.f) : CameraManager->GetActorForwardVector() * (CameraManager->GetCameraLocation() - GetActorLocation()).Size());
+		
+		if (UKismetSystemLibrary::LineTraceSingle(this, Start, Start + CameraManager->GetActorForwardVector() * 300.f, ETraceTypeQuery::TraceTypeQuery1, false, TArray<AActor*>{}, EDrawDebugTrace::None, OutHit, true))
+		{
+			if (auto* InteractedTarget = Cast<IMoriInteractionInterface>(OutHit.GetActor()))
+			{
+				if (InteractedTarget->TryToInteract())
+				{
+					Target = OutHit.GetActor();
+
+					if (bTryToInteract)
+						TryToInteract();
+
+					if (Target != PrevTarget)
+					{
+						if (auto* PrevInteractedTarget = Cast<IMoriInteractionInterface>(PrevTarget))
+							PrevInteractedTarget->EndHover();
+
+						PrevTarget = Target;
+					}
+					
+					return;
+				}
+			}
+		}
+
+		if (auto* InteractedTarget = Cast<IMoriInteractionInterface>(Target))
+		{
+			InteractedTarget->EndHover();
+			EndInteracting();
+
+			PrevTarget = Target = nullptr;
+		}
+	}
+}
+
+void AMoriCharacter::Interact()
+{
+	ServerInteracting(Target, this, false);
+
+	bTryToInteract = bInteracting = false;
+	Target = nullptr;
+	
+	if (auto* World = GetWorld())
+		World->GetTimerManager().UnPauseTimer(CheckInteractionTimerHandle);
+}
+
+void AMoriCharacter::BeginInteracting()
+{
+	ServerInteracting(Target, this, true);
+
+	bTryToInteract = false;
+	bInteracting = true;
+}
+
+void AMoriCharacter::EndInteracting()
+{
+	bInteracting = false;
+
+	if (auto* InteractedTarget = Cast<IMoriInteractionInterface>(Target))
+		InteractedTarget->EndInteracting();
+
+	if (auto* World = GetWorld())
+	{
+		World->GetTimerManager().PauseTimer(InteractionTimerHandle);
+		World->GetTimerManager().UnPauseTimer(CheckInteractionTimerHandle);
+	}
+}
+
+void AMoriCharacter::ServerInteracting_Implementation(AActor* InteractedTarget, ACharacter* Interactor, bool bBeginning)
+{
+	if (auto* Interactable = Cast<IMoriInteractionInterface>(InteractedTarget))
+		bBeginning ? Interactable->BeginInteracting(Interactor) : Interactable->Interact(Interactor);
+}
+
+void AMoriCharacter::ClientStopInteracting_Implementation()
+{
+	if (bInteracting)
+		EndInteracting();
+}
+
+void AMoriCharacter::ServerPickupWeapon_Implementation(TSubclassOf<AMoriPickedWeapon> WeaponClass, int32 AmmoNum)
+{
+
 }
--- Source/MementoMori/Public/Player/MoriCharacter.h
+++ Source/MementoMori/Public/Player/MoriCharacter.h
@@ -1,21 +1,30 @@
 #pragma once
 
+#include "MoriActionInterface.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "MoriCharacter.generated.h"
 
 UCLASS()
-class MEMENTOMORI_API AMoriCharacter : public ACharacter
+class MEMENTOMORI_API AMoriCharacter : public ACharacter, public IMoriActionInterface
 {
 	GENERATED_BODY()
 
 public:
 	AMoriCharacter();
 
+	virtual void PickupWeapon(TSubclassOf<class AMoriPickedWeapon> WeaponClass, int32 AmmoNum) override;
+
 protected:
+	virtual void PossessedBy(AController* NewController) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 private:
+	UFUNCTION(Client, Reliable)
+	void ClientInit();
+
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
 	void LookUp(float AxisValue);
@@ -29,6 +38,25 @@ private:
 
 	UFUNCTION(Server, Reliable)
 	void ServerRunning(bool bHaveToRun);
+
+	void Interacting();
+	void StopInteracting();
+
+	void TryToInteract();
+	void CheckInteracting();
+
+	void Interact();
+	void BeginInteracting();
+	void EndInteracting();
+
+	UFUNCTION(Server, Reliable)
+	void ServerInteracting(AActor* InteractedTarget, ACharacter* Interactor, bool bBeginning);
+
+	UFUNCTION(Client, Reliable)
+	void ClientStopInteracting();
+
+	UFUNCTION(Server, Reliable)
+	void ServerPickupWeapon(TSubclassOf<class AMoriPickedWeapon> WeaponClass, int32 AmmoNum);
 	
 private:
 	UPROPERTY(VisibleAnywhere)
@@ -46,4 +74,12 @@ private:
 	bool bUseFirstPersonView = true;
 	bool bCrouching = false;
 	bool bRunning = false;
+	bool bTryToInteract = false;
+	bool bInteracting = false;
+
+	FTimerHandle CheckInteractionTimerHandle;
+	FTimerHandle InteractionTimerHandle;
+
+	UPROPERTY(Replicated)
+	AActor* Target;
 };
